1874d926b5d895716f48b0cc002718cd1bce613e
Robin Janzen <rjjanzen@web.de>
2024-04-25 21:57:32 +0200

    feat: login page && simple navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:focus_timer/ui/pages/login_page.dart';
 import 'package:focus_timer/ui/themes/theme_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -35,53 +36,40 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.background,
         title: Text(widget.title),
+        centerTitle: true,
         actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 8),
-            child: ElevatedButton(
-              onPressed: () {
-                context.read<ThemeProvider>().toggleTheme();
-              },
-              child: Icon(context.read<ThemeProvider>().isDarkMode
-                  ? Icons.light_mode
-                  : Icons.dark_mode),
-            ),
-          )
+          GestureDetector(
+            onTap: () {
+              context.read<ThemeProvider>().toggleTheme();
+            },
+            child: Icon(context.read<ThemeProvider>().isDarkMode
+                ? Icons.light_mode
+                : Icons.dark_mode),
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const LoginPage()));
+            },
+            child: const Icon(Icons.login),
+          ),
+          const SizedBox(
+            width: 20,
+          ),
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have the button this many hello times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: const Center(
+        child: Text("Hello"),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
