8f117a2cdb33267bf391f2a5e69ea2721f7c067c
Robin Janzen <rjjanzen@web.de>
2024-04-25 22:18:43 +0200

    feat: gradient background

--- lib/ui/pages/timer_page.dart
+++ lib/ui/pages/timer_page.dart
@@ -7,38 +7,50 @@ class TimerPage extends StatelessWidget {
   const TimerPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        title: const Text("Focus Timer"),
-        centerTitle: true,
-        actions: [
-          GestureDetector(
-            onTap: () {
-              context.read<ThemeProvider>().toggleTheme();
-            },
-            child: Icon(context.read<ThemeProvider>().isDarkMode
-                ? Icons.light_mode
-                : Icons.dark_mode),
+    return Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Theme.of(context).colorScheme.background,
+              Theme.of(context).colorScheme.tertiary,
+            ],
           ),
-          const SizedBox(
-            width: 20,
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            title: const Text("Focus Timer"),
+            centerTitle: true,
+            actions: [
+              GestureDetector(
+                onTap: () {
+                  context.read<ThemeProvider>().toggleTheme();
+                },
+                child: Icon(context.read<ThemeProvider>().isDarkMode
+                    ? Icons.light_mode
+                    : Icons.dark_mode),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const LoginPage()));
+                },
+                child: const Icon(Icons.login),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+            ],
           ),
-          GestureDetector(
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const LoginPage()));
-            },
-            child: const Icon(Icons.login),
-          ),
-          const SizedBox(
-            width: 20,
-          ),
-        ],
-      ),
-      body: const Center(
-        child: Text("Hello"),
-      ),
-    );
+          body: const Center(child: Text("Hello")),
+        ));
   }
 }
